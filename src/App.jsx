efea22d9899bddde8327d418bd72b032f8597c4d
Napharat-ss <napharat.sroisom@gmail.com>
2025-08-09 16:54:25 +0700

    css nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "./components/Layouts/MainLayout";
+import MainLayout from "./Layouts/MainLayout";
+import Aboutus from "./views/Aboutus";
+import Contact from "./views/Contact";
 import Home from "./views/Home";
+import Menu from "./views/Menu";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: <MainLayout />,
     errorElement: (
       <div>
         <h1>404 - Page Not Found</h1>
       </div>
     ),
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "menu", element: <Menu /> },
+      { path: "about", element: <Aboutus /> },
+      { path: "contact", element: <Contact /> },
+    ],
   },
 ]);
 
--- src/components/landingpage/Navbar.jsx
+++ src/components/landingpage/Navbar.jsx
@@ -1,8 +1,62 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import { ShoppingBasket } from "lucide-react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <>
-    </>
-  )
+    <nav className="flex justify-between items-center py-4 px-4 lg:px-20 bg-amber-900 bg-opacity-10">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-light m-0 text-gray-300">
+        Obsidian<span className="text-yellow-600">Sip</span>
+      </h1>
+      <div className="flex gap-4 justify-center">
+        <Link to="/" className="nav-style">
+          HOME
+        </Link>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger to="" className="bg-none">
+                Menu
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink
+                  to="menu"
+                  className="block p-2 hover:bg-gray-100 rounded"
+                >
+                  Our Menu
+                </NavigationMenuLink>
+                <NavigationMenuLink
+                  href="/specials"
+                  className="block p-2 hover:bg-gray-100 rounded"
+                >
+                  Specials
+                </NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Link to="/" className="nav-style">
+          ABOUT US
+        </Link>
+        <Link to="/" className="nav-style">
+          CONTACT
+        </Link>
+      </div>
+
+      <div className="hidden md:flex gap-3 m-0">
+        <ShoppingBasket className="text-white" />
+        <button className="bg-amber-200 py-3 px-8 rounded-full border-none font-medium transition-all duration-500 hover:bg-yellow-700 hover:text-white cursor-pointer z-50">
+          Get started
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
