c9ea8eabe2257dd6aafed412767eb9ccdde10b15
Napharat-ss <napharat.sroisom@gmail.com>
2025-08-09 22:03:02 +0700

    add(mobilenavbar)

--- src/components/landingpage/Navbar.jsx
+++ src/components/landingpage/Navbar.jsx
@@ -1,60 +1,67 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
-import { ShoppingBasket } from "lucide-react";
+import { AlignRight, ShoppingBasket, X } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const mobileNav = [
+    { name: "HOME", path: "/" },
+    { name: "MENU", path: "menu" },
+    { name: "ABOUT US", path: "about" },
+    { name: "CONTACT", path: "contact" },
+  ];
   return (
-    <nav className="flex justify-between items-center py-4 px-4 lg:px-20 bg-amber-900 bg-opacity-10">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl font-light m-0 text-gray-300">
+    <nav className="flex justify-between items-center py-2 px-4 lg:px-20 bg-amber-900 bg-opacity-10">
+      <h1 className="text-2xl md:text-4xl lg:text-4xl m-0 text-gray-300">
         Obsidian<span className="text-yellow-600">Sip</span>
       </h1>
-      <div className="flex gap-4 justify-center">
+      {/*Desktop nav*/}
+      <div className="hidden md:flex gap-6 justify-center ">
         <Link to="/" className="nav-style">
           HOME
         </Link>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger to="" className="bg-none">
-                Menu
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink
-                  to="menu"
-                  className="block p-2 hover:bg-gray-100 rounded"
-                >
-                  Our Menu
-                </NavigationMenuLink>
-                <NavigationMenuLink
-                  href="/specials"
-                  className="block p-2 hover:bg-gray-100 rounded"
-                >
-                  Specials
-                </NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <Link to="/" className="nav-style">
+        <Link to="menu" className="nav-style">
+          MENU
+        </Link>
+        <Link to="about" className="nav-style">
           ABOUT US
         </Link>
-        <Link to="/" className="nav-style">
+        <Link to="contact" className="nav-style">
           CONTACT
         </Link>
       </div>
-
-      <div className="hidden md:flex gap-3 m-0">
-        <ShoppingBasket className="text-white" />
-        <button className="bg-amber-200 py-3 px-8 rounded-full border-none font-medium transition-all duration-500 hover:bg-yellow-700 hover:text-white cursor-pointer z-50">
-          Get started
-        </button>
+      <div className="hidden md:flex items-center gap-1 m-0">
+        <ShoppingBasket className="text-white w-14 h-7" />
+        <button className="getbutton">Get started</button>
       </div>
+
+      {/*mobile toggle nav */}
+      <button
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className="md:hidden text-white"
+      >
+        {isMenuOpen ? <X /> : <AlignRight />}
+      </button>
+      {/* mobile menu nav*/}
+      {isMenuOpen && (
+        <div className="absolute top-12 left-0 w-full bg-amber-800 bg-opacity-90 flex flex-col items-center gap-4 py-6 md:hidden z-50 ">
+          {mobileNav.map((item) => (
+            <Link
+              key={item.path}
+              to={item.path}
+              className="text-white text-lg hover:text-amber-400 cursor-pointer"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              {item.name}
+            </Link>
+          ))}
+
+          <div className="flex items-center gap-2">
+            <button className="getbutton" onClick={() => setIsMenuOpen(false)}>
+              Get started
+            </button>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
