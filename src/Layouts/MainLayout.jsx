fd4db24748f3eba9c753df8182bfb52a144f76f6
Amonrat <109088709+guzzle999@users.noreply.github.com>
2025-08-12 01:06:26 +0700

    completed forgotpwd component

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/landingpage/Navbar";
+import ForgotPassword from "../views/ForgotPassword";
 
 const MainLayout = () => {
   return (
