4fb871904cef0793bcac16fdb95fe98445de1cae
Napharat-ss <napharat.sroisom@gmail.com>
2025-08-16 13:35:06 +0700

    Add(newmenu)

--- src/components/landingpage/Newmenu.jsx
+++ src/components/landingpage/Newmenu.jsx
@@ -1,12 +1,56 @@
 import { SparklesText } from "@/components/magicui/sparkles-text";
+import { coldCoffeeList } from "../../data/ColdCoffee";
+import { dessertList } from "../../data/Dessert";
+import { hotCoffeeList } from "../../data/HotCoffee";
+import { MenuCard } from "../MenuCard";
 
 const Newmenu = () => {
+  const selectedIds = [108, 307, 207, 304];
+
+  const allProducts = [...hotCoffeeList, ...coldCoffeeList, ...dessertList];
+  const filteredProducts = allProducts.filter((product) =>
+    selectedIds.includes(product.id)
+  );
+
   return (
-    <section className="bg-[#FFF0D7]">
-      <SparklesText className="text-center font-libertinus py-10">
+    <section className="bg-[url('/bgnewmenu.jpg')] bg-cover">
+      <SparklesText className="text-center text-white font-libertinus py-10 text-4xl md:text-6xl">
         Our New Menu
       </SparklesText>
-      <div></div>
+
+      <div className="px-10 pb-10">
+        {/* Mobile & Tablet: scrollable */}
+        <div className="flex gap-6 overflow-x-auto md:overflow-x-auto xl:hidden">
+          {filteredProducts.map((product) => (
+            <div
+              key={product.id}
+              className="flex-shrink-0 w-72 md:w-1/2 xl:w-auto"
+            >
+              <MenuCard
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                price={product.price}
+                img={product.img}
+              />
+            </div>
+          ))}
+        </div>
+
+        {/* Desktop: grid 4 columns */}
+        <div className="hidden xl:grid xl:grid-cols-4 xl:gap-6">
+          {filteredProducts.map((product) => (
+            <MenuCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              description={product.description}
+              price={product.price}
+              img={product.img}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/components/landingpage/Why.jsx
@@ -0,0 +1,5 @@
+const Why = () => {
+  return <section></section>;
+};
+
+export default Why;
