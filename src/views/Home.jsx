b273f024183466e3fdadb791431d3cf26f78bd8d
Napharat-ss <napharat.sroisom@gmail.com>
2025-08-18 16:44:35 +0700

    fix(Home) : import detail

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,4 @@
-import Detailbean from "../components/landingpage/detailbean";
+import Detailbean from "../components/landingpage/Detailbean.jsx";
 import Herosection from "../components/landingpage/Herosection";
 import Newmenu from "../components/landingpage/Newmenu";
 import { ReviewCard } from "../components/landingpage/ReviewCard";
