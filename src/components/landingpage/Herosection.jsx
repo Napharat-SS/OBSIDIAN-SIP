58ab30e7f159bbade615ed876c478d40e9eea0ee
Napharat-ss <napharat.sroisom@gmail.com>
2025-09-09 11:23:05 +0700

    addfav

--- src/components/landingpage/Herosection.jsx
+++ src/components/landingpage/Herosection.jsx
@@ -10,24 +10,19 @@ export default function HeroSection() {
   const [currentVideo, setCurrentVideo] = useState(0);
   const navigate = useNavigate();
 
+  // วนเล่นวิดีโอทีละไฟล์
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentVideo((prev) => (prev + 1) % videos.length);
-    }, 7000);
+    }, 7000); // เปลี่ยนทุก 15 วิ (ปรับได้)
     return () => clearInterval(interval);
   }, []);
 
-  const goToMorningDuo = () => {
-    navigate("/menu/morning-duo");
-  };
-  const goToMenu = () => {
-    navigate("/menu");
-  };
-
   return (
     <section className="relative w-full h-screen overflow-hidden">
+      {/* วิดีโอ background */}
       <video
-        key={currentVideo[currentVideo]}
+        key={currentVideo[currentVideo]} // บังคับ re-render เวลาเปลี่ยนไฟล์
         src={videos[currentVideo]}
         autoPlay
         muted
@@ -35,9 +30,10 @@ export default function HeroSection() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       />
-
+      {/* Overlay มืดๆ */}
       <div className="absolute inset-0 bg-black/20" />
       {/* Content */}
+
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center px-4 translate-y-30">
         {/* Title */}
         <motion.h1
@@ -48,31 +44,33 @@ export default function HeroSection() {
         >
           Brewed with passion, Baked with Love
         </motion.h1>
+
         {/* Description */}
         <motion.p
           className="text-lg md:text-2xl mb-6 max-w-2xl drop-shadow-md text-gray-200 font-poppins"
           initial={{ opacity: 0, y: 40 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.3, duration: 3, ease: "easeOut" }}
+          transition={{ delay: 0.2, duration: 3, ease: "easeOut" }}
         >
           Special coffee & bakery, anywhere, anytime.
         </motion.p>
+
         {/* Buttons */}
         <motion.div
           className="flex gap-4 font-poppins"
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.1, duration: 3, ease: "easeOut" }}
+          transition={{ delay: 0.6, duration: 3, ease: "easeOut" }}
         >
           <button
-            className="px-6 py-3 rounded-3xl bg-[#c58c4ce6] text-white font-medium shadow-lg hover:bg-[#ddb07ee6] transition"
-            onClick={goToMorningDuo}
+            className="px-6 py-3 rounded-3xl bg-[#c58c4ce6] text-white font-medium shadow-lg hover:bg-[##ddb07ee6] transition"
+            onClick={() => navigate("/menu/morning-duo")}
           >
             Get Yours Today
           </button>
           <button
             className="px-6 py-3 rounded-3xl bg-transparent border border-white text-white font-medium hover:bg-white/20 transition"
-            onClick={goToMenu}
+            onClick={() => navigate("/menu")}
           >
             Browse Menu
           </button>
