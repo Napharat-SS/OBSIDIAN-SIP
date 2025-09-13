6c065985b5a3d2e7dd621eaf4a1878794cb07164
Supanat-pra <earthpra06@gmail.com>
2025-09-13 23:31:01 +0700

    profile(get,put,delete) connect to API

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,6 +1,23 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const API = "http://localhost:3030/user/profile";
 
 export default function Profile() {
+  const { user } = useAuth();
+
+  if (!user) {
+    return (
+      <div className="bg-gradient-to-r from-[#000000] to-[#341f01] min-h-screen flex items-center justify-center">
+        <div className="bg-neutral-900 p-8 rounded-xl shadow-lg w-full text-[#3F3C38] md:w-3/7 px-10 py-15">
+          <h4 className="text-3xl font-bold text-white text-center mb-4">
+            Please login first
+          </h4>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="bg-gradient-to-r from-[#000000] to-[#341f01] min-h-screen">
       <div className="p-5 md:p-10">
@@ -59,32 +76,32 @@ export default function Profile() {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mx-5 mb-6">
               <div>
                 <label
-                  for="first_name"
+                  htmlFor="firstname"
                   className="block  text-[#fdcf8e] text-sm font-medium mt-5 mb-2 uppercase"
                 >
                   First Name
                 </label>
                 <input
                   type="text"
-                  id="first_name"
-                  name="first_name"
-                  value={"Obsidian"}
+                  id="firstname"
+                  name="firstname"
+                  value={user.firstname}
                   readOnly
                   className="w-full  px-4 py-2 text-[#E8D9C6] border border-[#fff8ee] rounded-md focus:outline-none"
                 />
               </div>
               <div>
                 <label
-                  for="last_name"
+                  htmlFor="lastname"
                   className="block text-[#fdcf8e] text-sm font-medium mt-5 mb-2 uppercase"
                 >
                   Last Name
                 </label>
                 <input
                   type="text"
-                  id="last_name"
-                  name="last_name"
-                  value={"Sipper"}
+                  id="lastname"
+                  name="lastname"
+                  value={user.lastname}
                   readOnly
                   className="w-full px-4 py-2 text-[#E8D9C6] border border-[#fff8ee] rounded-md focus:outline-none"
                 />
@@ -100,7 +117,7 @@ export default function Profile() {
                   type="email"
                   id="email"
                   name="email"
-                  value={"asdf@gmail.com"}
+                  value={user.email}
                   readOnly
                   className="w-full px-4 py-2 text-[#E8D9C6] border border-[#fff8ee] rounded-md focus:outline-none"
                 />
@@ -108,23 +125,23 @@ export default function Profile() {
             </div>
             <div className="mx-5 mb-6">
               <label
-                for="tel"
+                htmlFor="phone"
                 className="block text-[#fdcf8e] text-sm font-medium mb-2 uppercase"
               >
                 Tel.
               </label>
               <input
                 type="tel"
-                id="tel"
-                name="tel"
-                value={"123456789"}
+                id="phone"
+                name="phone"
+                value={user.phone}
                 readOnly
                 className="w-full px-4 py-2 text-[#E8D9C6] border border-[#fff8ee] rounded-md focus:outline-none"
               />
             </div>
             <div className="mx-5 mb-6">
               <label
-                for="address"
+                htmlFor="address"
                 className="block text-[#fdcf8e] text-sm font-medium mb-2 uppercase"
               >
                 Address
@@ -133,11 +150,10 @@ export default function Profile() {
                 id="address"
                 name="address"
                 rows="4"
+                value={user.address}
                 readOnly
                 className="w-full px-4 py-2 text-[#E8D9C6] border border-[#fff8ee] rounded-md focus:outline-none resize-y"
-              >
-                99/999 Debaratna Rd, Bang Na Nuea, Bang Na, Bangkok 10260
-              </textarea>
+              ></textarea>
             </div>
           </div>
         </div>
@@ -145,3 +161,18 @@ export default function Profile() {
     </div>
   );
 }
+
+// const [user, setUser] = useState();
+
+// const fetchUser = async () => {
+//   try {
+//     const res = await axios.get(API);
+//     setUser(res.user);
+//   } catch (err) {
+//     console.error("Failed to fetch user:"), err;
+//   }
+// };
+
+// useEffect(() => {
+//   fetchUser();
+// }, []);
